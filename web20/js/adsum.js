b7a831b56760fd7da6e6560c9ab308708846e914
jbermudez1 <javierbermudez@MacBook-Pro-de-Javier-2.local>
2015-03-09 13:35:11 -0700

    agregando secundarias

--- web20/js/adsum.js
+++ web20/js/adsum.js
@@ -24,6 +24,23 @@ function bsolutions () {
 	 $("#otraining").load("soluciones/business-solution/otraining.html");
 	 $("#bistroft").load("soluciones/business-solution/bistroft.html");
 	 $("#kredits").load("soluciones/business-solution/kredits.html");
+}
+function sforsale () {
+	 $("#cupon").load("soluciones/solution-for-sales/cupon.html");
+	 $("#invoice").load("soluciones/solution-for-sales/invoice.html");
+	 $("#points").load("soluciones/solution-for-sales/points.html");
+	 $("#tiendactrl").load("soluciones/solution-for-sales/tiendactrl.html");
+	 $("#vstore").load("soluciones/solution-for-sales/vstore.html");
+}
+function apps () {
+	 $("#apps").load("soluciones/apps/apps.html");
 
 }
+function desarrollo () {
+	 $("#desarrollo").load("servicios/desarrollo-software/development.html");
 
+}
+function licencia () {
+	 $("#licencia").load("servicios/licenciamiento/licensing.html");
+
+}
